0e7c8ea7e64d8aa8262bbb548052014f34c02539
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 12:45:58 +0200

    feat: create HelloTest.tsx and src/components/Hello/index.ts

---
+++ src/components/Hello/HelloTest.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { trpClient } from '@/server/client'
+import React from 'react'
+
+export const HelloTest = ({option}: any) => {
+  const trpc = trpClient.main.hello.useQuery({name: option})
+  return (
+    <div className='text-2xl p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
+      <pre>
+        {JSON.stringify(trpc.data, null, 2)}
+      </pre>
+    </div>
+  )
+}
