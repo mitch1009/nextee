a1b016dcde211bffa9903520e16c8a87b7e80dcf
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 14:29:10 +0200

    feat: create src/server/trpc/routes/hello/index.ts and route.ts

---
+++ src/server/trpc/routes/hello/route.ts
@@ -0,0 +1,9 @@
+import { procedure, router } from "@/server/trpc/trpc";
+import { HelloService } from "./service";
+import { z } from "zod";
+export const helloRoute = router({
+    hello: procedure.input(z.object({name: z.string()})).query(({input}) => {
+        console.log(input)
+        return HelloService.hello(input)
+    })
+})
