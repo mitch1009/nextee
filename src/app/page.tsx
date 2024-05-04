bcb56b880a3669ed4d2f7b5a4b21f39c57c7362e
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 19:10:30 +0200

    update package.json, pnpm-lock.yaml, page.tsx and GraphqlPage.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@ import ActionsPage from "@/components/Hello/ActionsPage";
 import {GraphqlPage} from "@/components/Hello/GraphqlPage";
 import {TrpcPage} from "@/components/Hello/TrpcPage";
 import { HomePage } from "@/components/Home";
-
-export default function Home() {
+export default async function Home() {
+  const helloData= await fetch(`http://localhost:7766/api/graphql`,{
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST', 
+    body: JSON.stringify({query: `
+    query Query($name: String="Apollo Graphql") { hello(name: $name) }
+    `})
+  })
   return (
     <main className="flex min-h-screen flex-col items-center justify-center mx-auto max-w-7xl ">
       <HomePage  >
         <div className="">
           <ActionsPage key={1} />
-          <GraphqlPage key={2} />
+          <GraphqlPage key={2} data={await helloData.json()} />
           <TrpcPage/>
         </div>
 
--- src/components/Hello/GraphqlPage.tsx
+++ src/components/Hello/GraphqlPage.tsx
@@ -1,29 +1,13 @@
-'use client'
 import React from 'react'
-import { env } from 'process'
-export const  GraphqlPage=()=> {
-    const [data,setData] = React.useState<any|null>(null)
-    React.useEffect(() => {
-        fetch(`${env.AUTH_URL}/api/graphql`,
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    query: `query Query($name: String="graphql") { hello(name: $name) }
-                  `,
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        ).then((res) => res.json())
-            .then((data) => {
-                setData(data.data.hello)
-            })
-    })
- 
+export const GraphqlPage = ({ data }: any) => {
     return (
-        data && <div className='text-2xl rounded-md p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
-            {data?.data?.hello}
-        </div>
+        data ? <div className='text-2xl rounded-md p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
+            {data?.data.hello}
+
+        </div> :
+            <div className='text-2xl rounded-md p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
+                Loading... or nothing here {data}
+            </div>
+
     )
 }
