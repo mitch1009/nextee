e1e662a7990c8d0823d888ded4b2c5bed803dd28
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 20:00:23 +0200

    update 1 file and rename 2 files

--- src/server/graphql/modules/hello/mutations/index.ts
+++ src/server/graphql/modules/hello/mutations/index.ts
@@ -1,2 +1,2 @@
-export * from './client';
-export * from './server';
+export * from './requests';
+export * from './resolvers';
--- src/server/graphql/modules/hello/mutations/resolvers.ts
+++ src/server/graphql/modules/hello/mutations/resolvers.ts
@@ -1,5 +1,11 @@
 import { HelloService } from "@/server/core/hello";
 import { IHelloSchema } from "@/server/core/hello/types";
+/**
+ * Hello Mutation Resolvers
+ * @param _ Parent
+ * @param args Arguments
+ * @returns Object
+ */
 export const HelloMutations = {
     hello: (_: unknown, args: IHelloSchema) => HelloService.helloMutation(args)
 }
