f9734407aa3d4e1a6040862663fec8d7d16acc1f
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 17:06:18 +0200

    chore: update data

--- src/components/Navigation/MenuData.tsx
+++ src/components/Navigation/MenuData.tsx
@@ -1,9 +1,17 @@
 import {
     UserIcon, HomeIcon, AdjustmentsHorizontalIcon,
     BellAlertIcon, PencilSquareIcon, ShareIcon, WrenchIcon,
-    NewspaperIcon, Cog6ToothIcon
+    NewspaperIcon, Cog6ToothIcon,
+    BriefcaseIcon,
+    ChatBubbleLeftRightIcon,
+    CircleStackIcon,
+    ExclamationCircleIcon,
+    LightBulbIcon,
+    MagnifyingGlassPlusIcon,
+    PhotoIcon,
+    SpeakerWaveIcon
 } from '@heroicons/react/24/outline'
-import { INavigationProps } from './NavigationDto'
+import { INavigationLinks, INavigationProps } from './NavigationDto'
 
 export const userNavigation = [
     { title: 'Your profile', href: '/uzar/profile' },
@@ -11,7 +19,7 @@ export const userNavigation = [
 ]
 
 
-const ProfileData = [
+export const ProfileData = [
     { title: "Edit Profile", href: '/uzar/profile/edit-profile ', icon: PencilSquareIcon },
     { title: "Social Media", href: '/uzar/profile/social-media', icon: ShareIcon },
     { title: "Change Password", href: '/uzar/profile/change-password', icon: WrenchIcon },
@@ -19,6 +27,18 @@ const ProfileData = [
     { title: "Notification Settings", href: '/uzar/profile/notification-settings', icon: BellAlertIcon },
 ]
 
+
+export const AdminData: INavigationLinks[]|any = [
+    { title: "Getting Started", icon: NewspaperIcon, href: '' },
+    { title: "Documentation", icon: PhotoIcon, href: '' },
+    { title: "Campaigns", icon: BriefcaseIcon, href: '' },
+    { title: "Translation", icon: CircleStackIcon, href: '' },
+    { title: "Integrations", icon: ExclamationCircleIcon, href: '' },
+    { title: "Platform Setup", icon: SpeakerWaveIcon, href: '' },
+    { title: "Pre-fund", icon: LightBulbIcon, href: '' },
+    { title: "Users & Roles", icon: MagnifyingGlassPlusIcon, href: '' },
+    { title: "Reported Content", icon: ChatBubbleLeftRightIcon, href: '' },
+]
 
 export const MAIN_NAVIGATION_DATA: INavigationProps = {
     user: {
@@ -29,7 +49,7 @@ export const MAIN_NAVIGATION_DATA: INavigationProps = {
     admin: {
         home: { href: "/uzar", title: "News Feed", icon: NewspaperIcon },
         settings: { href: "/uzar/settings", title: "News Feed", icon: Cog6ToothIcon },
-        users: { href: "/uzar/users", title: "Profile", icon: UserIcon, submenu: ProfileData },
+        users: { href: "/uzar/users", title: "Profile", icon: UserIcon, submenu: AdminData },
     }
 
 }
