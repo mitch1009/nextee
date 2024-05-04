1a9a846de26897b4c1120382d37ad83eee24951d
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 12:48:06 +0200

    update main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,7 +4,6 @@ import {  procedure, router } from './trpc'
 
 export const appRouter = router({
     main: helloRoute,
-    hey: procedure.query(() => 'hello')
 })
 
 export type AppRouter = typeof appRouter;
