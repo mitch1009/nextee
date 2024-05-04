b9679d3620b39557fe794de76ca567651f5cb7b5
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 14:27:26 +0200

    chore: delete src/app/api/graphql/schema/index.ts and main.ts

--- src/app/api/graphql/schema/main.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from  'graphql-tag'
-export  const  typeDefs  =  gql`
-    type  Query {
-        hello: String
-    }
-    type Mutation {
-        hello(name: String): String
-    }
-    `
