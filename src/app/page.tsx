6019ff17bad6bed24c4ce8944182bec52680430f
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 19:46:41 +0200

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
 import ActionsPage from "@/components/Hello/ActionsPage";
-import {GraphqlPage} from "@/components/Hello/GraphqlPage";
-import {TrpcPage} from "@/components/Hello/TrpcPage";
+import { GraphqlPage } from "@/components/Hello/GraphqlPage";
+import { TrpcPage } from "@/components/Hello/TrpcPage";
 import { HomePage } from "@/components/Home";
-import { getClient } from "@/lib";
-import { helloRequest } from "@/server/graphql/modules";
+import { getHelloData } from "@/server/graphql/modules";
 export default async function Home() {
-  const {data} = await getClient().query({
-    query: helloRequest,
-    variables: {
-      name: "Apollo Graphql"
-    }
-  })
+const {data} = await getHelloData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-center mx-auto max-w-7xl ">
       <HomePage  >
         <div className="">
           <ActionsPage key={1} />
           <GraphqlPage key={2} data={data} />
-          <TrpcPage/>
+          <TrpcPage />
         </div>
 
       </HomePage>
