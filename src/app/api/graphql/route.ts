85172b56f24777877190c05677c826477489e69c
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 14:27:30 +0200

    update route.ts

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,14 +1,2 @@
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { ApolloServer } from '@apollo/server';
-import { resolvers } from './resolvers';
-import { typeDefs } from './schema';
-
-
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-});
-
-const handler = startServerAndCreateNextHandler(server);
-
+import {handler}  from '@/server/graphql';
 export { handler as GET, handler as POST };
