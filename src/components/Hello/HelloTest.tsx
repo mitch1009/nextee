5a7a47349cdee07f10ff098980749e10b2f7dc20
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 16:34:45 +0200

    update HelloTest.tsx

--- src/components/Hello/HelloTest.tsx
+++ src/components/Hello/HelloTest.tsx
@@ -2,13 +2,14 @@
 import { trpClient } from '@/server/trpc/client'
 import React from 'react'
 
-export const HelloTest = ({option}: any) => {
-  const trpc = trpClient.main.hello.useQuery({name: option})
+export const HelloTest = ({ option }: any) => {
+ const c=  trpClient.main.hello
+  const data = {
+    trpc: c.useQuery({ name: option }),
+  }
   return (
-    <div className='text-2xl p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
-      <pre>
-        {JSON.stringify(trpc.data, null, 2)}
-      </pre>
+    Object.entries(data).map(([k, v]:any) => <div key={k} className='text-2xl rounded-md p-8 border border-dashed mx-auto max-w-3xl bg-gray-100 my-8'>
+      {k==='trpc' ? v['data'] : v}
     </div>
-  )
+    ))
 }
