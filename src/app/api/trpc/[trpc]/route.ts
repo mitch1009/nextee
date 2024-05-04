c7b0ab27b86d6df59f127576d9703fed4d478ca5
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 12:45:49 +0200

    update route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { appRouter } from "@/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { auth ,db} from "@/lib";
+import { auth, db } from "@/lib";
+import { appRouter } from "@/server/main";
+import { createTRPCContext } from "@/server/context";
 
 /**
  * Handles the incoming request and returns the response from the fetchRequestHandler.
@@ -10,16 +11,11 @@ import { auth ,db} from "@/lib";
  * @return {Promise<NextResponse>} The response from the fetchRequestHandler.
  */
 const handler = async (req: NextRequest): Promise<NextResponse> => {
-  const session = await auth();
   const response = await fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: async () => ({
-      session: await auth(),
-      user: session?.user,
-      db: db,
-    }) as any,
+    createContext: async () => await createTRPCContext({ headers: req.headers }),
   }) as any;
 
   return response;
