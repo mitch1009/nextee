5271d8f87338bc05383f776cbecea5088f910857
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 14:28:35 +0200

    feat: create typeDevs.ts

---
+++ src/server/graphql/schema/typeDevs.ts
@@ -0,0 +1,9 @@
+import { gql } from  'graphql-tag'
+export  const  typeDefs  =  gql`
+    type  Query {
+        hello: String
+    }
+    type Mutation {
+        hello(name: String): String
+    }
+    `
