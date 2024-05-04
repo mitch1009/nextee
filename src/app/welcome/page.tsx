9e0f3798e5b68075ef5a5983524f1b0b9d75c78e
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-04 11:59:29 +0200

    update page.tsx

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,12 +1,11 @@
+import { Welcome } from '@/components/Welcome'
 
-
-import { OnBoardingPage } from '@/components/OnBoarding'
 import React from 'react'
 
 function page() {
   return (
     <div className='max-w-7xl mx-auto py-8'>
-      <OnBoardingPage/>
+      <Welcome/>
     </div>
   )
 }
