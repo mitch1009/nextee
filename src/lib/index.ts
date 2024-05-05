f5f1528de7cbb850f6fc374bd0878f935869d354
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-05 06:26:25 +0200

    create 2 files and update 2 files

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './apollo.config';
 export * from './auth';
 export * from './database';
+export * from './styles.config';
---
+++ src/lib/styles.config.ts
@@ -0,0 +1,1 @@
+export const clx = (...args: string[]) => args.filter(Boolean).join(" ")
