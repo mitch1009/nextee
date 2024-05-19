6b6dc4019ff72e1f9d7a007e7e68cd1790edba62
Mitch Chanza <mitch.chanza@gmail.com>
2024-05-19 22:01:20 +0200

    Add Cors through Middleware Fixes #10

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,29 @@
-import { auth } from "@/lib/auth"
+import { auth } from "@/lib/auth";
+import { Session } from "next-auth";
+import { NextResponse } from "next/server";
+import { env } from "process";
 
-export default auth((req) => { 
+export default auth(async (req) => {
   if (!req.auth && publicRoutes.includes(req.nextUrl.pathname)) {
-    const url = req.url.replace(req.nextUrl.pathname, "/auth/signin")
-    return Response.redirect(url)
+    const url = req.url.replace(req.nextUrl.pathname, "/auth/signin");
+    return Response.redirect(url);
   }
-})
+
+  // Implement login to allow some origins to access data on this app
+  if (req.headers.get("origin")) {
+    const allowedOrigins = (env.ALLOWED_ORIGINS ?? "").split(",");
+    const origin = req.headers.get("origin") ?? "";
+    if (allowedOrigins.includes(origin)) {
+      const session = (await auth()) as Session;
+      if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+    }
+  }
+});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", '/uzar'],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/uzar"],
+};
 
-export const publicRoutes = ['/uzar']
+export const publicRoutes = ["/uzar"];
