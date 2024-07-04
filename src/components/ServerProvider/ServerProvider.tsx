692fd48b2671b9c7835896f2928b4197933f83b6
Mitch Chimwemwe Chanza <hello@mitch.guru>
2024-07-04 09:00:07 +0200

    Update ServerProvider.tsx
    Signed-off-by: Mitch Chimwemwe Chanza <hello@mitch.guru>

--- src/components/ServerProvider/ServerProvider.tsx
+++ src/components/ServerProvider/ServerProvider.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { httpBatchLink } from '@trpc/react-query'
 import React, { useState } from 'react'
 import superjson from 'superjson'
+import { createWSClient, wsLink } from '@trpc/client'
 /**
  * Creates a server provider component that wraps the given children with a trpc client and a query client.
  *
@@ -12,13 +13,18 @@ import superjson from 'superjson'
  * @param {React.ReactNode} props.children - The children to be wrapped by the server provider.
  * @return {JSX.Element} The server provider component.
  */
-const ServerProvider = ({ children, url }: { url: string, children: React.ReactNode }) => {
+const ServerProvider = ({ children, urls }: { urls: { http: string, ws: string }, children: React.ReactNode }) => {
     const [queryCliet] = useState(() => new QueryClient())
+    const wslclient = createWSClient({ url: urls.ws })
     const [trpcClientInstance] = useState(trpClient.createClient({
         links: [
             httpBatchLink({
                 transformer: superjson,
-                url,
+                url: urls.http,
+            }),
+            wsLink({
+                transformer: superjson,
+                client: wslclient
             })
         ]
     }))
